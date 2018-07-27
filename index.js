643d27b44d311f5f5d785dc1d59dae11d40ce7c6
Shelby Trinca <shelby.trinca@gmail.com>
2018-07-27 18:10:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function johnLennonFActs(factsArray) {
   var index = 0;
   while (index < factsArray.length) {
     newArray.push(`${factsArray[index]}`+"!!!");
+    index++
   }
   return newArray;
 }
