6e459efe722c1a2f4c58e43bfcc99af8998a612b
Shelby Trinca <shelby.trinca@gmail.com>
2018-07-27 17:56:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = {};
+  for (var i=0; i < musicians.length(); i++) {
+    var add = musicians[i]+" plays "+ instruments[i];
+    newArray.push(add);
+  }
+  return newArray;
+}
